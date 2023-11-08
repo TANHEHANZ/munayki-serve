a3bec9436253b6100285002b1c7706fc712e3452
TANHEHANZ <hantach10@gmail.com>
2023-11-08 10:41:20 -0400

    modifi

--- controllers/login.js
+++ controllers/login.js
@@ -7,37 +7,26 @@ const validTokens = new Set();
 
 app.post("/login", async (req, res) => {
   const { correo, password } = req.body;
-  if (!correo || !password) {
-    return res.status(400).json({
-      error: "Correo y contraseña son obligatorios",
-    });
-  }
-  const user = await prisma.usuario.findFirst({
+  const login = await prisma.usuario.findFirst({
     where: {
       correo: correo,
+      password: password
     },
   });
-  if (!user) {
-    res.status(401).json({
-      error: "Usuario no encontrado",
+  if (!login) {
+    res.json({
+      message: "Usuario no autorizado",
+      error: "Usuario no autorizado",
     });
+    return;
   }
-  if (password !== user.password) {
-    return res.status(401).json({
-      error: "Contra incorrecta",
-    });
-  }
-  const userForToken = {
-    id: user.id,
-    username: user.correo,
-  };
-  const token = jwt.sign(userForToken, "123");
-  res.status(200).json({
-    name: user.nombre,
-    userName: user.correo,
-    token,
+  login.password = undefined;
+  res.json({
+    message: "Inicio de sesion correcto",
+    data: login,
   });
 });
+
 app.post("/logout", (req, res) => {
   const token = req.body.token;
   if (!validTokens.has(token)) {
