36f8ed162becf898b920853ebb4f63cf9625356d
TANHEHANZ <hantach10@gmail.com0>
2023-11-15 11:19:21 -0400

    controllermultimedia-prisma

--- controllers/multimedia.js
+++ controllers/multimedia.js
@@ -1,55 +1,81 @@
 const express = require("express");
-const {
-    PrismaClient
-} = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const app = express();
 const prisma = new PrismaClient();
 
-app.get('/multimedia', async (req, res) => {
-    const multimetida = await prisma.multimedia.findMany();
-    res.send(multimetida);
+app.get("/multimedia", async (req, res) => {
+  const multimetida = await prisma.multimedia.findMany({
+    include: {
+      usuario: true,
+      ubicacion: true,
+    },
+  });
+  res.send(multimetida);
 });
 
-app.post('/multimedia', async (req, res) => {
+// app.post("/multimedia", async (req, res) => {
+//   const multimedia = await prisma.multimedia.create({
+//     data: {
+//       foto: req.body.foto,
+//       AlertasUsuario: req.body.AlertasUsuario,
+//     },
+//   });
+//   res.json({
+//     message: "successully created",
+//     data: multimedia,
+//   });
+// });
+
+app.post("/Multimedia", async (req, res) => {
+  try {
     const multimedia = await prisma.multimedia.create({
-        data: {
-            foto: req.body.foto,
-            AlertasUsuario: req.body.AlertasUsuario,
-        }
+      data: {
+        foto: req.body.foto,
+        fecha: req.body.fecha,
+        usuarioId: req.body.usuarioId,
+        ubicacionId: req.body.ubicacionId,
+      },
     });
+
     res.json({
-        message: "successully created",
-        data: multimedia
-    })
-})
-app.put('/multimedia', async (req, res) => {
-    const id = parseInt(req.params.id);
-    const multimedia = await prisma.multimedia.update({
-        where: {
-            id
-        },
-        data: {
-            AlertasUsuario,
-            foto
-        },
+      message: "Multimedia creada con éxito",
+      data: multimedia,
     });
-    res.json({
-        message: "Successully update",
-        data: multimedia
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
     });
+  }
 });
 
-app.delete('/multimedia/:id',async(req,res)=>{
-    const id=parseInt(req.params.id);
-    const multimedia= await prisma.multimedia.delete({
-    where:{
-        id
-    }
-    });
-    res.json({
-        message:"successully delete",
-        data:multimedia
-    })
-})
+app.put("/multimedia", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const multimedia = await prisma.multimedia.update({
+    where: {
+      id,
+    },
+    data: {
+      AlertasUsuario,
+      foto,
+    },
+  });
+  res.json({
+    message: "Successully update",
+    data: multimedia,
+  });
+});
 
-module.exports=app
+app.delete("/multimedia/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const multimedia = await prisma.multimedia.delete({
+    where: {
+      id,
+    },
+  });
+  res.json({
+    message: "successully delete",
+    data: multimedia,
+  });
+});
+
+module.exports = app;
